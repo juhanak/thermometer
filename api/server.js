0b6673c3c31c97b63304f37ce3fa7d789e0fb0a6
Juhana Koski <juhana.koski@gmail.com>
2016-11-12 18:28:23 +0200

    fixes

--- api/server.js
+++ api/server.js
@@ -1,16 +1,24 @@
 var sql = require('mssql');
+var http = require('http')
+var port = process.env.PORT || 1337;
 
 var connectionString = process.env.mssqlconnection;
-var test = process.env.test;
-console.log(test);
 sql.connect(connectionString).then(function() {
 
-    new sql.Request().query('select * from device').then(function(recordset) {
-        console.dir(recordset);
-    }).catch(function(err) {
-        console.log(err) 
-    });
- 
 }).catch(function(err) {
     console.log(err)
 });
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    new sql.Request().query('select * from device').then(function(recordset) {
+        //console.dir(recordset);
+        res.end('request fine\n');
+    }).catch(function(err) {
+        res.end('request failed\n');
+    });
+
+
+  
+}).listen(port);
