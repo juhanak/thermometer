08a3660da85111d77eab0d0b345f6e7c2d3907bc
Juhana Koski <juhana.koski@gmail.com>
2016-11-12 18:14:23 +0200

    adds db support

--- api/server.js
+++ api/server.js
@@ -1,7 +1,8 @@
 var sql = require('mssql');
 
 var connectionString = process.env.mssqlconnection;
-
+var test = process.env.test;
+console.log(test);
 sql.connect(connectionString).then(function() {
 
     new sql.Request().query('select * from device').then(function(recordset) {
